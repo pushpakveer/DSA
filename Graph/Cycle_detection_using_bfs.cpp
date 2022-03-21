1f94413a41b76c696ed6bb79645774075d3272b0
Pushpak Veer <78746562+pushpakveer@users.noreply.github.com>
2022-03-21 23:58:45 +0530

    Update Cycle_detection_using_bfs.cpp

--- Graph/Cycle_detection_using_bfs.cpp
+++ Graph/Cycle_detection_using_bfs.cpp
@@ -11,55 +11,6 @@
 using namespace std;
 
 
-// vector<int>bfs( int n,vector<int>adj[]){
-//   vector<int>visited(n,0);
-//   vector<int>ans;
-//   queue<int>q;
-//   q.push(0);
-//   visited[0]=1;
-//   //apply here for loop to check non visted nodes..
-//   while(!q.empty()){
-
-//       int front=q.front();
-//       q.pop();
-//       ans.push_back(front);
-
-
-
-//      for(auto i:adj[front]){
-//      if(visited[i]==0){
-//          q.push(i);
-//          visited[i]=1;
-
-//           }
-//   }
-
-
-
-
-//   }
-
-
- 
-
-// return ans;
-
-
-
-
-// }
-
-// void dfs(int node,vector<int>&ans,vector<int>adj[],vector<int>&visited){
-//    ans.push_back(node);
-//    visited[node]=1;
-//    for(auto i:adj[node]){
-//     if(visited[i]==0){
-//       dfs(i,ans,adj,visited);
-//     }
-//    }
-
-
-// }
 
 bool checkcycle(int i,vector<int>&visited,vector<int>adj[]){
     queue<pair<int,int>>q;
@@ -111,34 +62,7 @@ adj[v].push_back(u);
 
 }
   vector<int>visited(n,0);
-// vector<int>answers=bfs(5,adj);
-// for(int i=0;i<answers.size();i++){
-//    cout<<answers[i]<<" ";
-// }
-// cout<<endl;
 
-// vector<int>ans;vector<int>answers=bfs(5,adj);
-// for(int i=0;i<answers.size();i++){
-//    cout<<answers[i]<<" ";
-// }
-// cout<<endl;
-//   vector<int>ans;
-
-// for(int i=0;i<n;i++){
-
-//   if(visited[i]==0){
-//          dfs(i,ans,adj,visited);
-//          visited[i]=1;
-
-//           }
-
-
-// }
-
-// for(int i=0;i<ans.size();i++){
-//    cout<<ans[i]<<" ";
-// }
-// cout<<endl;
 
 int flag=0;
 for(int i=0;i<visited.size();i++){
